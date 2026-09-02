b1d4e88e8dee0921db5e804ddf860b6af70df284
Krishna Vamshi <krishnavamshi81476@gmail.com>
2026-09-03 00:46:10 +0530

    add customer list page

--- service/models.py
+++ service/models.py
@@ -4,3 +4,6 @@ class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
--- service/views.py
+++ service/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . models import Customer
 
-# Create your views here.
+def customer_list(request):
+    customers = Customer.objects.all()
+
+    content = {
+        'customers': customers
+    }
+
+    return render(request, 'service/customer_list.html', content)
