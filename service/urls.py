9f3f73d53708969d58e90946c10ddbdb541d7ffa
Krishna Vamshi <krishnavamshi81476@gmail.com>
2026-09-03 23:33:06 +0530

    Add customer detail page

--- service/urls.py
+++ service/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('customers/', views.customer_list, name='customer_list'),
     path('customers/add/', views.customer_create, name='customer_create'),
+    path('customers/<int:id>/', views.customer_detail, name='customer_detail'),
 ]
--- service/views.py
+++ service/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . models import Customer
 from . forms import CustomerForm
 
@@ -28,3 +28,12 @@ def customer_create(request):
     }
 
     return render(request, 'service/customer_form.html', content)
+
+def customer_detail(request, id):
+    customer = get_object_or_404(Customer, id=id)
+
+    content = {
+        'customer': customer
+    }
+
+    return render(request, 'service/customer_detail.html', content)
